ec1b8c2ccf0f5167a78c7453c37ae99965180c63
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2023-10-31 15:40:23 +0700

    update source

---
+++ Admin Template/tairo_1.3/.app/nuxt.config.ts
@@ -0,0 +1,31 @@
+export default defineNuxtConfig({
+  extends: [
+    /**
+     * This extends the base Tairo layer.
+     *
+     * Alternatively you can use the following:
+     * 'github:cssninjaStudio/tairo/layers/xxx#v1.0.0'
+     *
+     * And set GIGET_AUTH=<github_token> in your .env file
+     *
+     * This would allows you to create an empty git repository
+     * with only your source code and no demo.
+     */
+    '../layers/tairo-layout-sidebar',
+    '../layers/tairo',
+
+    /**
+     * Uncomment the following line to add the Tairo Layout Sidebar layer
+     */
+    // '../layers/tairo-layout-sidebar',
+  ],
+
+  /**
+   * Load local font with @fontsource packages
+   * @see https://fontsource.org/
+   */
+  css: [
+    '@fontsource-variable/inter/index.css',
+    '@fontsource-variable/karla/index.css',
+  ],
+})
